ec5c991239a595f340bd77a5721db019732f7873
uranberisha <uranberisha2@gmail.com>
2021-04-25 16:11:54 +0200

    - create AddPlaceFragment

--- app/src/main/java/com/urani/favoriteplaces/ui/main/AddPlaceFragment.kt
+++ app/src/main/java/com/urani/favoriteplaces/ui/main/AddPlaceFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,30 +15,23 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.urani.favoriteplaces.R
-import com.urani.favoriteplaces.database.entities.Place
-import com.urani.favoriteplaces.databinding.FragmentMainBinding
-import com.urani.favoriteplaces.ui.main.adapter.PlacesAdapter
+import com.urani.favoriteplaces.databinding.FragmentAddPlaceBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment : Fragment(), OnMapReadyCallback{
-
-    private lateinit var binding: FragmentMainBinding
-
+class AddPlaceFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+    private lateinit var binding: FragmentAddPlaceBinding
     private val viewModel: MainViewModel by viewModels()
 
-    private lateinit var googleMap: GoogleMap
-    val zoomLevel = 13.0f
-    private lateinit var mapFragment: SupportMapFragment
-
     private lateinit var mContext: Context
-
-    private lateinit var placesAdapter: PlacesAdapter
-
     override fun onAttach(context: Context) {
         mContext = context
         super.onAttach(context)
     }
+
+    private lateinit var googleMap: GoogleMap
+    val zoomLevel = 13.0f
+    private lateinit var mapFragment: SupportMapFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,55 +39,40 @@ class MainFragment : Fragment(), OnMapReadyCallback{
     ): View {
         // Inflate the layout for this fragment
         binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+            DataBindingUtil.inflate(inflater, R.layout.fragment_add_place, container, false)
         binding.fragment = this
 
-        placesAdapter =
-            PlacesAdapter(mContext, ArrayList()) { callback ->
-                zoomMapsToInitialState(LatLng(callback.latitude, callback.latitude))
-            }
-
-        binding.groupsRecyclerView.adapter = placesAdapter
-
-//        val place1 = Place(0,42.6697152, 21.1440927, "https://dev-youlo.lb.noexislab.cloud/images/35602364-6a1e-40de-9044-93664c4b0f52.jpeg")
-//        viewModel.insert(place1)
-//
-//        val place2= Place(0,42.657152, 21.1340927, "https://dev-youlo.lb.noexislab.cloud/images/35602364-6a1e-40de-9044-93664c4b0f52.jpeg")
-//        viewModel.insert(place2)
-
-        observerLiveData()
 
         return binding.root
     }
 
-    private fun observerLiveData() {
-        viewModel.getAllFavoritePlaces().observe(viewLifecycleOwner, Observer { lisOffPlaces ->
-            if (lisOffPlaces.isNullOrEmpty()) {
-                placesAdapter.onAddPlaces(ArrayList())
-            } else {
-                placesAdapter.onAddPlaces(lisOffPlaces as MutableList<Place>)
-            }
-        })
+    fun onBackButtonClick(view: View?) {
+        activity?.onBackPressed()
+    }
+
+    fun onEditPlaceButtonClick(view: View?) {
 
     }
 
-
-
-    fun onMenuButtonClick(view: View?) {
+    fun onTakePhotoClicked(view: View?) {
 
     }
 
-    fun onAddPostButtonClick(view: View?) {
-        activity?.supportFragmentManager?.let {
-            it.beginTransaction().add(R.id.fragment_container_layout, AddPlaceFragment())
-            .addToBackStack(null).commit();
-        }
+    fun onPasteLinkClicked(view: View?) {
+
+    }
+
+    fun onDeleteButtonClick(view: View?) {
+
     }
 
     override fun onMapReady(map: GoogleMap?) {
         binding.mapProgressBar.visibility = View.GONE
         map?.let {
             googleMap = map
+            googleMap.setOnMapLongClickListener(this)
+            googleMap.uiSettings.isZoomControlsEnabled = true
+
             googleMap.moveCamera(
                 CameraUpdateFactory.newLatLngZoom(
                     LatLng(42.6697152, 21.1440927),
@@ -105,6 +82,14 @@ class MainFragment : Fragment(), OnMapReadyCallback{
 
 
         }
+    }
+
+
+    override fun onMapLongClick(latLng: LatLng?) {
+        googleMap.clear()
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap.addMarker(MarkerOptions().position(latLng!!))
+        zoomMapsToInitialState(latLng)
     }
 
     private fun zoomMapsToInitialState(latLng: LatLng) {
